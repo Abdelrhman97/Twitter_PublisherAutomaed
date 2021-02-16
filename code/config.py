d8c732911ccc95337308a5cbbf60bc621527ed8b
Abdelrhman shaban <51994083+Abdelrhman97@users.noreply.github.com>
2021-02-16 20:16:22 +0200

    Update config.py

--- code/config.py
+++ code/config.py
@@ -1,14 +1,14 @@
-quotes_base_url = 'https://www.goodreads.com/quotes/tag' 
-gecko_driver = 'driver/geckodriver.exe' 
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'
-page_load_timeout = 30
+quotes_base_url = '' 
+gecko_driver = '' 
+user_agent = ''
+page_load_timeout = 
 Headless = False
-mongo_url = 'cluster0.tlpaw.mongodb.net/Quotes?retryWrites=true&w=majority'
-mongo_username = 'Abdelrhman'
-mongo_pass = '01027096167'
+mongo_url = ''
+mongo_username = ''
+mongo_pass = ''
 
 twitter_login_page = 'https://twitter.com/login/'
-twitter_email = '01551742133'
-twitter_pass = '01027096167' 
-twitter_cookies_path = 'twitter_cookies.json'
-twitter_url = 'https://twitter.com'
+twitter_email = ''
+twitter_pass = '' 
+twitter_cookies_path = ''
+twitter_url = ''
